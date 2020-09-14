e213fe684c350af783f73bd3b5a20227ef43c167
jang-hj <guswlalf@naver.com>
2020-09-14 14:10:58 +0900

    [jeagaebal]
    upmuController 메서드추가
    
    left 메뉴 경로추가
    
    approval폴더 생성 및 하위 html파일 생성

--- Jaegaebal/src/main/java/kr/or/jaegaebal/controller/UpmuController.java
+++ Jaegaebal/src/main/java/kr/or/jaegaebal/controller/UpmuController.java
@@ -10,7 +10,27 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class UpmuController {
+	
+	//결재함화면연결
+	@GetMapping("/appBox")
+	public String appBox() {
+		return "approval/appBox";
+	}
+	
+	//임시저장화면연결
+	@GetMapping("/storageBox")
+	public String storageBox() {
+		return "approval/storageBox";
+	}
+	
 
+	//기안하기화면연결
+	@GetMapping("/appWrite")
+	public String appWrite() {
+		return "approval/appWrite";
+	}
+
+	//출퇴근화면이동
 	@GetMapping("/checkInOut")
 	public String checkInOut() {
 		return "upmu/checkInOut";
