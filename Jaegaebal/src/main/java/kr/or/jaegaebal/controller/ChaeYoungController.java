0964c7f3b340fab78913f9177e1bcc9e586e40b6
hjiiiin0314 <khjin950314@gmail.com>
2020-09-14 13:37:46 +0900

    [Jaegaebal] cyboard 게시판 생성 file : chaeyoung/cyboard.html

--- Jaegaebal/src/main/java/kr/or/jaegaebal/controller/ChaeYoungController.java
+++ Jaegaebal/src/main/java/kr/or/jaegaebal/controller/ChaeYoungController.java
@@ -1,6 +1,8 @@
 package kr.or.jaegaebal.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * 담당자 : 김형진
@@ -10,4 +12,11 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChaeYoungController {
 
+	@GetMapping("cyboard")
+	public String cyBoard(Model model) {
+		
+		model.addAttribute("title", "구인공고게시판");
+		
+		return "chaeyoung/cyboard";
+	}
 }
