3f3bad102f95b4002f37e0e7d195c3884f143ada
DanBi <DanBi@DESKTOP-1DITHVH>
2020-09-16 22:41:48 +0900

    [김형진]
    date : 2020-09-16 22:40
    detail : 지원버튼을 눌렀을 때 모달에 값 전달 및 채용마감이 지나면 지원 못하게 경고창 출력

---
+++ Jaegaebal/src/main/java/kr/or/jaegaebal/dto/ChaeYoungApplicant.java
@@ -0,0 +1,49 @@
+package kr.or.jaegaebal.dto;
+
+public class ChaeYoungApplicant {
+
+	 private String appEmail; //지원자 이메일
+	 private String appNumber; //구인공고번호
+	 private String appPassward; //지원자 비밀번호
+	 private String appName; //지원자이름
+	 private String appPhone; //지원자 핸드폰번호
+	 private String appRegDate; //지원날짜
+	public String getAppEmail() {
+		return appEmail;
+	}
+	public void setAppEmail(String appEmail) {
+		this.appEmail = appEmail;
+	}
+	public String getAppNumber() {
+		return appNumber;
+	}
+	public void setAppNumber(String appNumber) {
+		this.appNumber = appNumber;
+	}
+	public String getAppPassward() {
+		return appPassward;
+	}
+	public void setAppPassward(String appPassward) {
+		this.appPassward = appPassward;
+	}
+	public String getAppName() {
+		return appName;
+	}
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+	public String getAppPhone() {
+		return appPhone;
+	}
+	public void setAppPhone(String appPhone) {
+		this.appPhone = appPhone;
+	}
+	public String getAppRegDate() {
+		return appRegDate;
+	}
+	public void setAppRegDate(String appRegDate) {
+		this.appRegDate = appRegDate;
+	}
+	 
+	 
+}
