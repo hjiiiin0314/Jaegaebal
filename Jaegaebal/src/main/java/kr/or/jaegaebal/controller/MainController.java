30bd7da9b1b369806b3e45d5c69198e8128825a2
jang-hj <guswlalf@naver.com>
2020-09-29 11:39:18 +0900

    [jaegaebal - 장현지]
    file : MainController.java
    date : 20.09.29
    detail : 충돌확인

--- Jaegaebal/src/main/java/kr/or/jaegaebal/controller/MainController.java
+++ Jaegaebal/src/main/java/kr/or/jaegaebal/controller/MainController.java
@@ -66,8 +66,7 @@ public class MainController {
 				httpsession.setAttribute("SSTAFFNUM", staffInfo.getStaffNum());
 				httpsession.setAttribute("SSTAFFNAME", staffInfo.getStaffName());
 				httpsession.setAttribute("SJOJICNAME", staffInfo.getJojicName());
-				httpsession.setAttribute("SJOJICCODE", staffInfo.getJojicCode());				
-				
+				httpsession.setAttribute("SJOJICCODE", staffInfo.getJojicCode());								
 			}
 			
 			result = 1;
